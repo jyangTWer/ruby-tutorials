493bbc6aefbd04c52b27bf631b13195dfa2eae9d
YANG Jing <jyang@thoughtworks.com>
2016-08-07 18:46:57 +0800

    [JY] - add raise two exception test

--- lib/line_feed.rb
+++ lib/line_feed.rb
@@ -1,26 +1,29 @@
 class LineFeed
     def wrap text, maxCol
-      words = text.split(' ')
-      concate_sentence_with_line_feed words, maxCol
+        raise ArgumentError, 'Please enter a positive number' if maxCol.is_a?(String) || maxCol < 1
+        words = text.split(' ')
+        concate_sentence_with_line_feed words, maxCol
     end
 
     def concate_sentence_with_line_feed words, maxCol
+        line_feed_texts = []
+        line_feed_texts = get_sentence words, maxCol
+        line_feed_texts.join('\n')
+    end
+
+    def get_sentence words, maxCol
         final_texts = []
         current_line = ""
         texts = []
-        while words.length > 0
-          word = words.shift
-
-          if current_line.length + word.length + 1 > maxCol
-            final_texts.push(current_line)
-            texts = []
-          end
-
-          texts.push(word)
-          current_line = texts.join(' ')
+        words.each do |word|
+            if current_line.length + word.length + 1 > maxCol
+                final_texts.push(current_line)
+                texts = []
+            end
+            texts.push(word)
+            current_line = texts.join(' ')
         end
-
         final_texts.push(current_line) if texts.length > 0
-        final_texts.join('\n')
+        final_texts
     end
 end
--- spec/line_feed_spec.rb
+++ spec/line_feed_spec.rb
@@ -17,6 +17,23 @@ describe 'Line Feed' do
 			expect(result).to eq ""
 		end
 
+		it 'should be empty when text is empty' do
+			result = lineFeed.wrap "", 10
+			expect(result).to eq ""
+		end
+
+		it 'should get ArgumentError error when maxCol is string' do
+			expect {
+				lineFeed.wrap "", "10"
+			}.to raise_error(ArgumentError)
+		end
+
+		it 'should get ArgumentError error when maxCol is less than 1' do
+			expect {
+				lineFeed.wrap "", 0
+			}.to raise_error(ArgumentError)
+		end
+
 		let(:lineFeed){ LineFeed.new }
 	end
 end
