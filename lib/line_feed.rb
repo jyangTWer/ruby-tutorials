53fe7991c56c7223e5270a7f0f6f8e53699e65dc
YANG Jing <jyang@thoughtworks.com>
2016-08-07 08:56:58 +0800

    [JY] - extract a method called concate_sentence_with_line_feed

--- lib/line_feed.rb
+++ lib/line_feed.rb
@@ -1,11 +1,15 @@
 class LineFeed
     def wrap text, maxCol
-      characters = text.split(' ')
-      final_texts = []
-      current_line = ""
-      texts = []
-      while characters.length > 0
-          word = characters.shift
+      words = text.split(' ')
+      concate_sentence_with_line_feed words, maxCol
+    end
+
+    def concate_sentence_with_line_feed words, maxCol
+        final_texts = []
+        current_line = ""
+        texts = []
+        while words.length > 0
+          word = words.shift
 
           if current_line.length + word.length + 1 > maxCol
             final_texts.push(current_line)
@@ -14,9 +18,9 @@ class LineFeed
 
           texts.push(word)
           current_line = texts.join(' ')
-      end
+        end
 
-      final_texts.push(current_line) if texts.length > 0
-      final_texts.join('\n')
+        final_texts.push(current_line) if texts.length > 0
+        final_texts.join('\n')
     end
 end
