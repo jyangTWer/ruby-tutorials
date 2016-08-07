eecf5517221722633a4f06ac1494c9b47a232b11
YANG Jing <jyang@thoughtworks.com>
2016-08-08 00:04:32 +0800

    [JY] - number to words and some test

--- lib/number.rb
+++ lib/number.rb
@@ -1,14 +1,21 @@
 class Number
-	MAPPING = {11=>"eleven", 12=>"twelve", 13=>"thirteen"}
+	MAPPING = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "fourty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
+	UNITS = {0=>"hundred", 1=>'thousand', 2=>"million", 3=>"billion", 4=>"trillion"}
 	def num_to_words number
-		raise ArgumentError, 'Please enter a positive number' if number.is_a?(String)
+		number = number.to_i if number.is_a?(String)
 		parts = split_num_to_parts number
-		parts = parts.reverse
-		text = ""
-		parts.each do |part|
-			text = MAPPING[part] 
+		words = []
+		length_of_parts = parts.length
+		parts.each_with_index do |part, index|
+			groups = []
+			groups = get_number part
+			words.push('(and)') if words.length > 0 && groups.length > 0
+			words = words + groups
+			if index < length_of_parts - 1
+				words.push(UNITS[length_of_parts-index-1]) 
+			end
 		end
-		text + ' dollars.'
+		words.join(' ') + ' dollars.'
 	end
 
 	def split_num_to_parts number
@@ -19,6 +26,29 @@ class Number
 			parts.push(temp)
 		end
 
-		parts
+		parts.reverse
+	end
+
+	def get_number part
+		digt_place = part%10
+		last_two_number = part%100
+		decade_place = last_two_number/10
+		hundred_place = part/100
+		words = []
+		if hundred_place > 0 
+			words.push(MAPPING[hundred_place])
+			words.push(UNITS[0])
+		end
+
+		if last_two_number != 0
+			words.push('(and)') if words.length > 0
+			if decade_place < 2
+				words.push(MAPPING[last_two_number])
+			else
+				words.push(MAPPING[last_two_number-digt_place])
+				words.push(MAPPING[digt_place]) if digt_place > 1
+			end
+		end
+		words
 	end
 end
--- spec/number_spec.rb
+++ spec/number_spec.rb
@@ -2,15 +2,34 @@ require 'spec_helper'
 
 describe 'Number' do
 	context '#num_to_words' do
-		it "" do
-			result = number.num_to_words 11
-			expect(result).to eq "eleven dollars."
+		it "has one digit" do
+			result = number.num_to_words "3"
+			expect(result).to eq "three dollars."
 		end
 
-		it 'should get ArgumentError error when maxCol is less than 1' do
-			expect {
-				number.num_to_words "123"
-			}.to raise_error(ArgumentError)
+		it "has two digit" do
+			result = number.num_to_words "13"
+			expect(result).to eq "thirteen dollars."
+		end
+
+		it "has three digit which doesn't include decade place" do
+			result = number.num_to_words "103"
+			expect(result).to eq "one hundred (and) three dollars."
+		end
+
+		it "has three digit" do
+			result = number.num_to_words "123"
+			expect(result).to eq "one hundred (and) twenty three dollars."
+		end
+
+		it "has four digit" do
+			result = number.num_to_words "1003"
+			expect(result).to eq "one thousand (and) three dollars."
+		end
+
+		it "has four digit whose last two digit is 30" do
+			result = number.num_to_words "1030"
+			expect(result).to eq "one thousand (and) thirty dollars."
 		end
 
 		let(:number){ Number.new }
